929a56c26623010723c1ee2b01bb9f25e2a9432a
Nehal Ingole <nehal712521@gmail.com>
2022-03-03 10:42:57 +0530

    Test

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SIH_App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome To Our Sign Up"),
+        ),
+      ),
+      drawer: Drawer(),
+
+
+    );
+
+  }
+
+}
